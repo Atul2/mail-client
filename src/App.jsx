ffc7a078db316fe3299e1ddd2345a4e22f619195
Atul2 <atulasrani12@gmail.com>
2021-01-08 17:37:31 +0530

    First Commit on project mail-client Signed-off-by: Atul2 <atulasrani12@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Mailbox from "./Mailbox";
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Outlook Mail
+     </nav>
+      <Mailbox />
+    </>
+  );
+}
+
+export default App;
